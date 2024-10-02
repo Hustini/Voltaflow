30e3c5cfcd3df0c8c7aefeb77dc9f8ea24e993bb
Hustini <ejdersalih02@gmail.com>
2024-10-02 11:17:42 +0200

    can get all files names, start times, end times and the duration

--- SDATData.py
+++ SDATData.py
@@ -1,34 +1,25 @@
 import os
 import xml.etree.ElementTree as ET
+from datetime import datetime
 
 
 def sdat_data(directory='SDAT-Files'):
     # Load the XML content from a file
-    tree = ET.parse(f'{directory}/20190313_093127_12X-0000001216-O_E66_12X-LIPPUNEREM-T_ESLEVU121963_-279617263.xml')
-    root = tree.getroot()
+    all_files = [f for f in os.listdir(directory) if os.path.isfile(os.path.join(directory, f))]
 
-    # Define the namespace
-    namespaces = {'rsm': 'http://www.strom.ch'}
+    for i in all_files:
+        # Parse the XML string
+        namespace = {'rsm': 'http://www.strom.ch'}
+        tree = ET.parse(os.path.join(directory, i))
+        root = tree.getroot()
 
-    # Find the DocumentID within the header
-    document_id = root.find('.//rsm:DocumentID', namespaces=namespaces).text
+        # Extract values
+        start_datetime_str= root.find('.//rsm:StartDateTime', namespace).text
+        end_datetime_str = root.find('.//rsm:EndDateTime', namespace).text
 
-    # Find all Observation tags
-    observations = root.findall('.//rsm:Observation', namespaces=namespaces)
+        start_datetime = datetime.fromisoformat(start_datetime_str[:-1])  # Remove 'Z' before conversion
+        end_datetime = datetime.fromisoformat(end_datetime_str[:-1])  # Remove 'Z' before conversion
 
-    sequences = []
-    # Iterate through each Observation and print its contents
-    for obs in observations:
-        sequence = obs.find('.//rsm:Sequence', namespaces=namespaces).text
-        volume = obs.find('.//rsm:Volume', namespaces=namespaces).text
-        sequences.append([sequence, volume])
+        print(f'{i}, Start: {start_datetime}, End: {end_datetime}, Duration: {end_datetime - start_datetime}')
 
-    return document_id, sequences
-
-
-data = sdat_data()
-minute = 15
-print(f'DocumentID: {data[0]}')
-for i in data[1]:
-    print(f'\tMinute {minute}, Volume: {i[1]}')
-    minute += 15
+sdat_data()
